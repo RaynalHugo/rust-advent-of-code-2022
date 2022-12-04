c6fcf95d9995ebc84ff4ba20323428b9df875ee5
Hugo Raynal <raynalhugo@gmail.com>
2022-12-04 19:08:05 +0100

    day 04

---
+++ src/day_04_task_02.rs
@@ -0,0 +1,62 @@
+#[allow(dead_code)]
+
+pub fn solve(input: &str) -> usize {
+    input
+        .split('\n')
+        .filter(|shift| {
+            let assignments: Vec<_> = shift
+                .split(',')
+                .map(|assignment| {
+                    assignment
+                        .split('-')
+                        .map(|period| period.parse::<u8>().unwrap())
+                        .collect::<Vec<_>>()
+                })
+                .collect();
+
+            let first_elf_assignment = assignments.first().unwrap();
+            let first_elf_assignment_range =
+                first_elf_assignment.first().unwrap()..=first_elf_assignment.get(1).unwrap();
+
+            let second_elf_assignment = assignments.get(1).unwrap();
+            let second_elf_assignment_range =
+                second_elf_assignment.first().unwrap()..=second_elf_assignment.get(1).unwrap();
+
+            if first_elf_assignment_range.contains(&second_elf_assignment.first().unwrap())
+                || first_elf_assignment_range.contains(&second_elf_assignment.get(1).unwrap())
+            {
+                return true;
+            }
+
+            if second_elf_assignment_range.contains(&first_elf_assignment.first().unwrap())
+                || second_elf_assignment_range.contains(&first_elf_assignment.get(1).unwrap())
+            {
+                return true;
+            }
+
+            false
+        })
+        .count()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::solve;
+    use std::fs;
+
+    #[test]
+    fn example() {
+        let input = fs::read_to_string("./inputs/day_04_example.txt")
+            .expect("couldn't find file ./inputs/day_04_example.txt");
+
+        assert_eq!(solve(&input), 4);
+    }
+
+    #[test]
+    fn given_input() {
+        let input = fs::read_to_string("./inputs/day_04_input.txt")
+            .expect("couldn't find file ./inputs/day_04_input.txt");
+
+        assert_eq!(solve(&input), 865);
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -3,3 +3,5 @@ mod day_01_task_02;
 mod day_02_task_01;
 mod day_03_task_01;
 mod day_03_task_02;
+mod day_04_task_01;
+mod day_04_task_02;
